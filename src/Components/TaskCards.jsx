7aab13c6fd2fda7c9aba3f8da17a7873ffb8fcd3
shashank8565 <shhashankrajput0992@gmail.com>
2025-03-15 02:38:29 +0530

    fixed imports

--- src/Components/TaskCards.jsx
+++ src/Components/TaskCards.jsx
@@ -13,7 +13,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
+import { Input } from "@/Components/ui/input";
 
 import { Textarea } from "./ui/textarea";
 
